20dc0bd7c243a26c8261d5961da448cf1f6c8f2d
Miles Cameron <miles.cameron.nv@gmail.com>
2025-09-28 20:22:12 -0500

    edit final retirement logic

--- money_model.py
+++ money_model.py
@@ -12,25 +12,37 @@ class MoneyAgent(mesa.Agent):
         # Pass the parameters to the parent class.
         super().__init__(unique_id, model)
         self.wealth = 10
+        self.retired= False
+
+    def assess_retirement(self):
+        """Decide if the agent should retire this step."""
+        if (not self.retired
+            and self.model.steps_remaining >= self.model.cutoff_step
+            and self.wealth >= (self.model.steps_remaining+3)):
+            self.retired = True
 
     def exchange(self):
-        # Verify agent has some wealth
         if self.wealth > 0:
+            # Pick a recipient: only non-retired agents are eligible
+            eligible = [a for a in self.model.agents if not a.retired and a is not self]
 
-            other_agent = self.random.choice(self.model.agents)
-
-            if other_agent is not None:
+            if eligible:
+                other_agent = self.random.choice(eligible)
                 other_agent.wealth += 1
-                self.wealth -= 1
+
+            # Retired agents still spend, but "into the void" if no eligible recipient
+            self.wealth -= 1
 
 
 class MoneyModel(mesa.Model):
     """A model with some number of agents."""
 
-    def __init__(self, n, seed=None):
+    def __init__(self, n, steps, seed=None):
         super().__init__(seed=seed)
         self.num_agents = n
         self.schedule = RandomActivation(self)
+        self.steps_remaining = steps
+        self.cutoff_step = steps*0.05
 
         for i in range(self.num_agents):
             agent = MoneyAgent(i+1, self)
@@ -38,16 +50,18 @@ class MoneyModel(mesa.Model):
 
     def step(self):
         """Advance the model by one step."""
+        self.agents.do("assess_retirement")
         self.agents.shuffle_do("exchange")
+        self.steps_remaining-=1
 
 all_wealth = []
 for _ in range(100):
 
     agents = 50
-    steps = 50
+    steps = 100
 
     # Run the model
-    model = MoneyModel(agents)
+    model = MoneyModel(agents,steps)
     for _ in range(steps):
         model.step()
 
